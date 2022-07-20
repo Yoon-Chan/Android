8afad1403e0186a4feafd10df12831b663aae948
Yoon-Chan <vsvx13@naver.com>
2022-07-21 01:13:36 +0900

    카카오 화면 만들기 위한 모듈 생성

--- chap06/app/src/main/java/com/example/chap06/MainActivity.kt
+++ chap06/app/src/main/java/com/example/chap06/MainActivity.kt
@@ -4,17 +4,39 @@ import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import com.example.chap06.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //바인딩 객체 획등
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //액티비티 화면 출력
         setContentView(R.layout.activity_main)
 
+        //뷰 객체 이용
+        binding.visibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.VISIBLE
+        }
+        binding.invisibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.INVISIBLE
+        }
+//
+//        val txtView1: TextView = findViewById(R.id.text1)
+//        val button1 = findViewById<Button>(R.id.button1)
+//
+//        button1.setOnClickListener {
+//            it.visibility = View.INVISIBLE
+//        }
 //
 //        //이름 문자열 출력 TextView 생성
 //        val name = TextView(this).apply {
