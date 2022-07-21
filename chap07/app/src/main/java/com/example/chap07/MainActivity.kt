5a9ac69abe783ed4e1e45932eddeacd57925e11b
Yoon-Chan <vsvx13@naver.com>
2022-07-22 04:58:15 +0900

    chap07 FrameLayout examples

--- chap07/app/src/main/java/com/example/chap07/MainActivity.kt
+++ chap07/app/src/main/java/com/example/chap07/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.chap07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.chap07.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //뷰 바인딩
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.button.visibility = View.INVISIBLE
+            binding.imageView.visibility = View.VISIBLE
+        }
+
+        binding.imageView.setOnClickListener {
+            binding.button.visibility = View.VISIBLE
+            binding.imageView.visibility = View.INVISIBLE
+        }
     }
 }
