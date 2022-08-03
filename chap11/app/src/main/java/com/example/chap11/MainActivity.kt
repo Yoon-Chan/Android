ebb674f27656bd2b634724add8fdc1bdd7732d8d
Chan <vsvx13@naver.com>
2022-08-04 05:26:08 +0900

    11장 액션바 예제

---
+++ chap11/app/src/main/java/com/example/chap11/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.chap11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //업 버튼 보이게 하는 구문
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    // 업 버튼 누를 때 실행되는 함수
+    override fun onSupportNavigateUp(): Boolean {
+        Log.d("chan", "onSupportNavigateUp")
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val menuItem = menu?.findItem(R.id.menu3)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("Not yet implemented")
+                //검색어 변경 이벤트
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("Not yet implemented")
+                // 키보드 검색 버튼을 클릭한 순간의 이벤트
+                return true
+            }
+
+        })
+//        val menuItem1 : MenuItem? = menu?.add(0,0,0, "menu1")
+//        val menuItem2 : MenuItem? = menu?.add(0,1,0, "menu2")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId){
+//            0 -> {
+//                Log.d("chan", "menu1 click")
+//                true
+//            }
+//
+//            1->{
+//                Log.d("chan", "menu2 click")
+//                true
+//            }
+//
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+}
